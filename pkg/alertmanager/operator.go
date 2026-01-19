7ac187ec63d5eeebf2596cf31b4cdc46429f82f0
Jayapriya Pai <slashpai9@gmail.com>
2026-01-19 14:45:59 +0530

    alertmanager: add webhookURL secret validation to checkMSTeamsConfigs (#8294)
    Validate that the webhookURL secret exists in checkMSTeamsConfigs,
    consistent with checkMSTeamsV2Configs. This prevents operator
    degradation when an AlertmanagerConfig references a missing secret.
    
    Signed-off-by: Jayapriya Pai <slashpai9@gmail.com>
    (cherry picked from commit 8a8faa867ed0f25d69b827d16682313be6189e4f)

--- pkg/alertmanager/operator.go
+++ pkg/alertmanager/operator.go
@@ -1555,6 +1555,10 @@ func checkMSTeamsConfigs(
 			return err
 		}
 
+		if _, err := store.GetSecretKey(ctx, namespace, config.WebhookURL); err != nil {
+			return err
+		}
+
 		if err := configureHTTPConfigInStore(ctx, config.HTTPConfig, namespace, store); err != nil {
 			return err
 		}
